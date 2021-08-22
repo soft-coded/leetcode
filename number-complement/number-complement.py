57fca292f4a7a283d2109098142183151bb2c343
Shrutanten <68508049+soft-coded@users.noreply.github.com>
2021-08-23 01:29:24 +0530

    Time: 28 ms (81.80%), Space: 14.1 MB (69.49%) - LeetHub

--- number-complement/number-complement.py
+++ number-complement/number-complement.py
@@ -1,6 +1,5 @@
 class Solution:
     def findComplement(self, num: int) -> int:
-        n = list(bin(num)[2:])
-        for i in range(len(n)):
-          n[i] = '1' if n[i] == '0' else '0'
-        return int(''.join(n), 2)
+      inv = ~0
+      while inv & num: inv <<= 1
+      return ~inv ^ num
