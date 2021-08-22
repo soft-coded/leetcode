2e4dbc06937a049bf7813cd19b4b2df2481f06d2
Shrutanten <68508049+soft-coded@users.noreply.github.com>
2021-08-23 02:12:16 +0530

    Time: 32 ms (76.20%), Space: 14.3 MB (41.16%) - LeetHub

--- construct-the-rectangle/construct-the-rectangle.py
+++ construct-the-rectangle/construct-the-rectangle.py
@@ -1,5 +1,5 @@
 class Solution:
     def constructRectangle(self, area: int) -> List[int]:
         w = int(area ** 0.5)
-        while (area / w) % 1: w -= 1
+        while area % w: w -= 1
         return [area // w, w]
