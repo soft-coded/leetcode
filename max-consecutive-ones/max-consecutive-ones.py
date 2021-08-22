d5096ca3b8e095720dcb1b5a6d0cd80d73cfece2
Shrutanten <68508049+soft-coded@users.noreply.github.com>
2021-08-23 02:01:50 +0530

    Time: 348 ms (71.90%), Space: 14.6 MB (19.65%) - LeetHub

--- max-consecutive-ones/max-consecutive-ones.py
+++ max-consecutive-ones/max-consecutive-ones.py
@@ -5,7 +5,7 @@ class Solution:
       for i in range(len(nums)):
         if nums[i] == 1:
           cur += 1
-        elif nums[i] == 0:
+        else:
           mx = max(mx, cur)
           cur = 0
       mx = max(mx, cur)
