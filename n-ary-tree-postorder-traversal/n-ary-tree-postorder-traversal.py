3d417d5b9b64fb0a1374b2c1c01046ff711d2719
Shrutanten <68508049+soft-coded@users.noreply.github.com>
2021-08-24 22:33:51 +0530

    Time: 56 ms (23.81%), Space: 16.2 MB (25.40%) - LeetHub

---
+++ n-ary-tree-postorder-traversal/n-ary-tree-postorder-traversal.py
@@ -0,0 +1,22 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def postorder(self, root: 'Node') -> List[int]:
+      st = [root]
+      ans = []
+      def po(node):
+        if not node: return
+        if node.children:
+          i = 0
+          while i < len(node.children):
+            po(node.children[i])
+            i += 1
+        ans.append(node.val)
+      po(root)
+      return ans
