239a3ca8703ed78dcbed196a546fe7c4684281a1
Shrutanten <68508049+soft-coded@users.noreply.github.com>
2021-08-24 22:35:22 +0530

    Time: 52 ms (58.38%), Space: 15.9 MB (98.94%) - LeetHub

--- n-ary-tree-postorder-traversal/n-ary-tree-postorder-traversal.py
+++ n-ary-tree-postorder-traversal/n-ary-tree-postorder-traversal.py
@@ -12,11 +12,10 @@ class Solution:
       ans = []
       def po(node):
         if not node: return
-        if node.children:
-          i = 0
-          while i < len(node.children):
-            po(node.children[i])
-            i += 1
+        i = 0
+        while i < len(node.children):
+          po(node.children[i])
+          i += 1
         ans.append(node.val)
       po(root)
       return ans
