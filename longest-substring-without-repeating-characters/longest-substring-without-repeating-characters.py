0281509920107d74d82a98903b1d00897a02471e
Shrutanten <68508049+soft-coded@users.noreply.github.com>
2021-08-30 23:37:49 +0530

    Time: 72 ms (47.97%), Space: 14.4 MB (54.59%) - LeetHub

---
+++ longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -0,0 +1,12 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+      mx = 0
+      prev = 0
+      d = {}
+      for i in range(len(s)):
+        if s[i] in d and d[s[i]] >= prev:
+          mx = max(mx, i - prev)
+          prev = d[s[i]] + 1
+        d[s[i]] = i
+      return max(mx, len(s) - prev)
+      
