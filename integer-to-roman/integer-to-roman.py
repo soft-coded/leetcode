279980ea95a4a1c1d4a67fab1c2cadc80daf3ced
Shrutanten <68508049+soft-coded@users.noreply.github.com>
2021-09-01 15:45:03 +0530

    Time: 42 ms (88.03%), Space: 14.3 MB (59.92%) - LeetHub

---
+++ integer-to-roman/integer-to-roman.py
@@ -0,0 +1,7 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+      m = ["", "M", "MM", "MMM"]
+      c = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
+      x = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
+      i = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
+      return m[num//1000] + c[(num%1000)//100] + x[(num%100)//10] + i[num%10]
