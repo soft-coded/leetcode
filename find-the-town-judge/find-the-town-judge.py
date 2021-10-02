05ff8577791fed0a0e31ea8714ad40383ffab7cf
Shrutanten <68508049+soft-coded@users.noreply.github.com>
2021-10-02 23:49:25 +0530

    Time: 720 ms (86.51%), Space: 19 MB (62.14%) - LeetHub

---
+++ find-the-town-judge/find-the-town-judge.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+      d = {}
+      for i in range(1, n + 1):
+        d[i] = set()
+      for item in trust:
+        d[item[0]].add(item[1])
+      juj = None
+      for key, val in d.items():
+        if not val:
+          if juj: return -1
+          juj = key
+          break
+      for key, val in d.items():
+        if key != juj and juj not in val:
+          return -1
+      return juj
