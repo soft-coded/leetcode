fccdc3bcb6e033d7e9c50bf14c9c5ed1a2bb7388
Shrutanten <68508049+soft-coded@users.noreply.github.com>
2022-06-09 19:33:17 +0530

    Time: 41 ms (48.92%), Space: 13.8 MB (53.78%) - LeetHub

--- 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.py
+++ 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.py
@@ -1,4 +1,11 @@
+def is_palin(s):
+  n = len(s) - 1
+  for i in range(math.ceil(len(s) / 2)):
+    if s[i] != s[n - i]:
+      return False
+  return True
+
 class Solution:
     def removePalindromeSub(self, s: str) -> int:
-      if s == s[::-1]: return 1
+      if is_palin(s): return 1
       return 2
