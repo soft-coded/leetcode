1587de2679ef7f2aa58dbbdba3a26046d8e39eb1
Shrutanten <68508049+soft-coded@users.noreply.github.com>
2022-06-12 17:28:57 +0530

    Time: 82 ms (17.31%), Space: 14.4 MB (88.31%) - LeetHub

---
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.py
@@ -0,0 +1,14 @@
+class Solution:
+    def searchMatrix(self, mat: List[List[int]], t: int) -> bool:
+      rows = len(mat) - 1
+      cols = len(mat[0]) - 1
+      i = 0
+      j = cols
+      while i < rows + 1 and j >= 0:
+        if mat[i][j] == t:
+          return True
+        elif mat[i][j] < t:
+          i += 1
+        else:
+          j -= 1
+      return False
