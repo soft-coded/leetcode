38c69986aa666ba08d258685fa974207927c08c7
Shrutanten <68508049+soft-coded@users.noreply.github.com>
2022-06-12 18:29:18 +0530

    Time: 113 ms (78.88%), Space: 17.9 MB (58.82%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -3,8 +3,7 @@ class Solution:
       d = {}
       for item in strs:
         item_s = "".join(sorted(item))
-        if item_s in d:
-          d[item_s].append(item)
-        else:
-          d[item_s] = [item]
+        if item_s not in d:
+          d[item_s] = []
+        d[item_s].append(item)
       return d.values()
