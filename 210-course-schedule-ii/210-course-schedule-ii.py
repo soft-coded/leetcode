bf3cda4ab131fe49831670ab08adf9e2eb086e67
Shrutanten <68508049+soft-coded@users.noreply.github.com>
2022-06-16 15:22:10 +0530

    Time: 170 ms (33.25%), Space: 16.9 MB (48.32%) - LeetHub

---
+++ 210-course-schedule-ii/210-course-schedule-ii.py
@@ -0,0 +1,35 @@
+class Solution:
+    def findOrder(self, courses: int, prereqs) -> List[int]:
+      graph = [[] for i in range(courses)]
+      visited = [0 for i in range(courses)]
+      ans = []
+      
+      for a, b in prereqs:
+        graph[a].append(b)
+        
+      def dfs(node):
+        if visited[node] == 1:
+          return True
+        if visited[node] == -1:
+          return False
+        
+        visited[node] = -1
+        
+        while graph[node]:
+          dep = graph[node].pop()
+          no_cycle = dfs(dep)
+          if not no_cycle:
+            return False
+          
+        visited[node] = 1
+        ans.append(node)
+        return True
+      
+      for i in range(courses):
+        if not dfs(i):
+          return []
+      
+      return ans
+            
+            
+            
