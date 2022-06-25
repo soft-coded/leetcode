b65445307e2b4a8ad19c3501b7d24012d7e278b2
Shrutanten <68508049+soft-coded@users.noreply.github.com>
2022-06-25 11:52:55 +0530

    Time: 62 ms (51.36%), Space: 13.9 MB (24.59%) - LeetHub

--- 31-next-permutation/31-next-permutation.py
+++ 31-next-permutation/31-next-permutation.py
@@ -1,36 +1,23 @@
+def reverse(st, end, arr):
+  while st < end:
+    arr[st], arr[end] = arr[end], arr[st]
+    st += 1
+    end -= 1
+
 class Solution:
     def nextPermutation(self, nums: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-        def check(s, m):
-            z = 0
-            if s != sorted(s, reverse = True):
-                return False
-            else:
-                for x in range(len(s) - 1, -1, -1):
-                    if s[x] > m:
-                        return x + 1
-        temp = 0
-        c = 1
-        for x in range(len(nums) - 2, -1, -1):
-            if nums[x] >= nums[x + 1]:
-                c = c + 1
-                continue
-            else:
-                z = check(nums[x + 1:], nums[x])
-                if z != False and x >= 0:
-                    temp = nums[x]
-                    nums[x] = nums[x + z]
-                    nums[x + z] = temp
-                nums[x + 1:] = nums[x + 1:][::-1]
-                break
-        if c == len(nums) :            
-            start = 0            
-            end = len(nums) - 1            
-            while start<end:                
-                temp = nums[start]                
-                nums[start] = nums[end]                
-                nums[end]=temp                
-                start+=1                
-                end-=1
+      n = len(nums)
+      i = n - 2
+      while i >= 0 and nums[i] >= nums[i + 1]:
+        i -= 1
+      
+      if i >= 0:
+        j = n - 1
+        while nums[i] >= nums[j]:
+          j -= 1
+        
+        nums[j], nums[i] = nums[i], nums[j]
+      
+      reverse(i + 1, n - 1, nums)
+      
+      return nums
