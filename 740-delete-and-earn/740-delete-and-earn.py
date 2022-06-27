e5f9c89cc6900e6224a2534f9a8732453197354f
Shrutanten <68508049+soft-coded@users.noreply.github.com>
2022-06-27 22:51:53 +0530

    Time: 175 ms (21.96%), Space: 16 MB (76.54%) - LeetHub

---
+++ 740-delete-and-earn/740-delete-and-earn.py
@@ -0,0 +1,15 @@
+class Solution:
+    def deleteAndEarn(self, nums: List[int]) -> int:
+        points = {}
+        max_number = 0
+        for num in nums:
+            points[num] = points.get(num, 0) + num
+            max_number = max(max_number, num)
+        
+        two_back = 0
+        one_back = points.get(1, 0)
+        
+        for num in range(2, max_number + 1):
+            two_back, one_back = one_back, max(one_back, two_back + points.get(num, 0))
+
+        return one_back
