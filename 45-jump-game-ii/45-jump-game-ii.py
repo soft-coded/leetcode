9216cbe160bc04abf32a773593e87125f7bdf9e8
Shrutanten <68508049+soft-coded@users.noreply.github.com>
2022-06-28 22:47:43 +0530

    Time: 198 ms (62.65%), Space: 15 MB (58.56%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.py
+++ 45-jump-game-ii/45-jump-game-ii.py
@@ -2,11 +2,11 @@ class Solution:
     def jump(self, nums: List[int]) -> int:
       if len(nums) == 1: return 0
       mx_reach = steps = nums[0]
-      jumps = 0
+      jumps = 1
       for i in range(1, len(nums) - 1):
         mx_reach = max(mx_reach, i + nums[i])
         steps -= 1
         if steps == 0:
           jumps += 1
           steps = mx_reach - i
-      return jumps + 1
+      return jumps
