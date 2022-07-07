20559a91fff36df47926d129c16eeb27a9d5d59f
Shrutanten <68508049+soft-coded@users.noreply.github.com>
2022-07-08 00:04:10 +0530

    Time: 189 ms (18.36%), Space: 14.1 MB (98.93%) - LeetHub

---
+++ 51-n-queens/51-n-queens.py
@@ -0,0 +1,22 @@
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+      res = []
+      self.dfs([-1]*n, 0, [], res)
+      return res
+    
+    def dfs(self, nums, index, path, res):
+        if index == len(nums):
+            res.append(path)
+            return 
+        for i in range(len(nums)):
+            nums[index] = i
+            if self.valid(nums, index):  # pruning
+                tmp = "." * len(nums)
+                self.dfs(nums, index + 1, path +[tmp[:i] + "Q" + tmp[i + 1:]], res)
+
+    # check whether nth queen can be placed in that column
+    def valid(self, nums, n):
+        for i in range(n):
+            if abs(nums[i] - nums[n]) == n - i or nums[i] == nums[n]:
+                return False
+        return True
