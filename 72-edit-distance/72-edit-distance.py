3a7278b8b5a06f52c584448a1726526f4ca75542
Shrutanten <68508049+soft-coded@users.noreply.github.com>
2022-07-17 16:09:49 +0530

    Time: 359 ms (14.86%), Space: 16.4 MB (87.88%) - LeetHub

--- 72-edit-distance/72-edit-distance.py
+++ 72-edit-distance/72-edit-distance.py
@@ -1,7 +1,7 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
-        
-        def recur(i, j, memo):
+        memo = {}
+        def recur(i, j):
           if i < 0 and j < 0:
             return 0
           if i < 0:
@@ -14,10 +14,31 @@ class Solution:
             return memo[memo_str]
           
           if word1[i] == word2[j]:
-            return recur(i - 1, j - 1, memo)
+            return recur(i - 1, j - 1)
           
-          ans = min(recur(i - 1, j, memo), recur(i, j - 1, memo), recur(i - 1, j - 1, memo)) + 1
+          ans = min(recur(i - 1, j), recur(i, j - 1), recur(i - 1, j - 1)) + 1
           memo[memo_str] = ans
           return ans
+          
+        return recur(len(word1) - 1, len(word2) - 1)
+          
+#         n = len(word1)
+#         m = len(word2)
+#         dp = [[0] * (m + 1) for _ in range(n + 1)]
         
-        return recur(len(word1) - 1, len(word2) - 1, {})
+#         for i in range(1, m + 1):
+#           dp[0][i] = m
+#         for i in range(1, n + 1):
+#           dp[i][0] = n
+          
+#         for i in range(1, n + 1):
+#           for j in range(1, m + 1):
+#             if word1[i - 1] == word2[j - 1]:
+#               dp[i][j] = dp[i - 1][j - 1]
+#             else:
+#               dp[i][j] = min(dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1]) + 1
+        
+#         return dp[n][m]
+        
+          
+          
