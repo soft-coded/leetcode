76467bea274106b40127eaedf759dcc81a0d2f79
Shrutanten <68508049+soft-coded@users.noreply.github.com>
2022-07-17 16:17:13 +0530

    Time: 342 ms (18.34%), Space: 17.6 MB (37.22%) - LeetHub

--- 72-edit-distance/72-edit-distance.py
+++ 72-edit-distance/72-edit-distance.py
@@ -1,44 +1,44 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
-        memo = {}
-        def recur(i, j):
-          if i < 0 and j < 0:
-            return 0
-          if i < 0:
-            return j + 1
-          if j < 0:
-            return i + 1
+#         memo = {}
+#         def recur(i, j):
+#           if i < 0 and j < 0:
+#             return 0
+#           if i < 0:
+#             return j + 1
+#           if j < 0:
+#             return i + 1
           
-          memo_str = f"{i},{j}"
-          if memo_str in memo:
-            return memo[memo_str]
+#           memo_str = f"{i},{j}"
+#           if memo_str in memo:
+#             return memo[memo_str]
           
-          if word1[i] == word2[j]:
-            return recur(i - 1, j - 1)
+#           if word1[i] == word2[j]:
+#             return recur(i - 1, j - 1)
           
-          ans = min(recur(i - 1, j), recur(i, j - 1), recur(i - 1, j - 1)) + 1
-          memo[memo_str] = ans
-          return ans
+#           ans = min(recur(i - 1, j), recur(i, j - 1), recur(i - 1, j - 1)) + 1
+#           memo[memo_str] = ans
+#           return ans
           
-        return recur(len(word1) - 1, len(word2) - 1)
+#         return recur(len(word1) - 1, len(word2) - 1)
           
-#         n = len(word1)
-#         m = len(word2)
-#         dp = [[0] * (m + 1) for _ in range(n + 1)]
+        n = len(word1)
+        m = len(word2)
+        dp = [[0] * (m + 1) for _ in range(n + 1)]
         
-#         for i in range(1, m + 1):
-#           dp[0][i] = m
-#         for i in range(1, n + 1):
-#           dp[i][0] = n
+        for i in range(1, m + 1):
+          dp[0][i] = i
+        for i in range(1, n + 1):
+          dp[i][0] = i
           
-#         for i in range(1, n + 1):
-#           for j in range(1, m + 1):
-#             if word1[i - 1] == word2[j - 1]:
-#               dp[i][j] = dp[i - 1][j - 1]
-#             else:
-#               dp[i][j] = min(dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1]) + 1
+        for i in range(1, n + 1):
+          for j in range(1, m + 1):
+            if word1[i - 1] == word2[j - 1]:
+              dp[i][j] = dp[i - 1][j - 1]
+            else:
+              dp[i][j] = min(dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1]) + 1
         
-#         return dp[n][m]
+        return dp[n][m]
         
           
           
