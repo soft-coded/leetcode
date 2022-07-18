acbf0b24325ce3228ede340e4e1f97a29198c5e5
Shrutanten <68508049+soft-coded@users.noreply.github.com>
2022-07-18 13:36:14 +0530

    Time: 3220 ms (33.42%), Space: 483.5 MB (5.04%) - LeetHub

---
+++ 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.py
@@ -0,0 +1,59 @@
+def is_palin(s, st, end):
+  while st < end:
+    if s[st] != s[end]:
+      return False
+    st += 1
+    end -= 1
+  return True
+
+# class Solution:
+#   def minCut(self, s: str) -> int:
+#     memo = {}
+#     def recur(i):
+#       if i == len(s):
+#         return 0
+#       if i in memo:
+#         return memo[i]
+        
+#       memo[i] = math.inf
+#       for j in range(i, len(s)):
+#         if is_palin(s, i, j):
+#           memo[i] = min(memo[i], recur(j + 1) + 1)
+        
+#       return memo[i]
+    
+#     return recur(0) - 1
+
+#     n = len(s)
+#     dp = [0] * (n + 1)
+
+#     for i in range(n - 1, -1, -1):
+#       dp[i] = math.inf
+#       for j in range(i, len(s)):
+#         if is_palin(s, i, j):
+#           dp[i] = min(dp[i], dp[j + 1] + 1)
+        
+#     return dp[0] - 1
+      
+class Solution:
+    def minCut(self, s: str) -> int:
+        n = len(s)
+        
+        @lru_cache(None)
+        def isPalindrome(l, r):  # l, r inclusive
+            if l >= r: return True
+            if s[l] != s[r]: return False
+            return isPalindrome(l+1, r-1)
+        
+        @lru_cache(None)
+        def dp(i):  # s[i..n-1]
+            if i == n:
+                return 0
+            ans = math.inf
+            for j in range(i, n):
+                if (isPalindrome(i, j)):
+                    ans = min(ans, dp(j+1) + 1)
+            return ans
+        
+        return dp(0) - 1
+      
