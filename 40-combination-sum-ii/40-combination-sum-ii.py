50155b912cc75a86b77d9b2023522305a8cc91d6
Shrutanten <68508049+soft-coded@users.noreply.github.com>
2022-07-19 16:46:27 +0530

    Time: 40 ms (99.58%), Space: 13.9 MB (58.17%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.py
+++ 40-combination-sum-ii/40-combination-sum-ii.py
@@ -1,21 +1,26 @@
 class Solution:
-    def combinationSum2(self, nums: List[int], target: int) -> List[List[int]]:
-        ans = []
-        nums.sort()
-        
-        def bt(nums, t, cur, ind):
-          if t == 0:
-            ans.append(cur)
-            return
-          if t < 0:
-            return
-          
-          for i in range(ind, len(nums)):
-            if i > ind and nums[i] == nums[i - 1]:
-              continue
-            if nums[i] > t:
-              break
-            bt(nums, t - nums[i], cur + [nums[i]], i + 1)
-        
-        bt(nums, target, [], 0)
-        return ans
+  def combinationSum2(self, nums: List[int], target: int):
+    ans = []
+    nums.sort()
+
+    def bt(nums, t, cur, ind):
+      if t == 0:
+        ans.append(cur)
+        return
+      if t < 0:
+        return
+
+      for i in range(ind, len(nums)):
+        if i > ind and nums[i] == nums[i - 1]:
+          continue
+        if nums[i] > t:
+          break
+        bt(nums, t - nums[i], cur + [nums[i]], i + 1)
+
+    bt(nums, target, [], 0)
+    return ans
+  
+  
+  
+  
+  
