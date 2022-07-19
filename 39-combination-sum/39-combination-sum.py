2a049eb1b1e054f6b7697b550a774ab693649df6
Shrutanten <68508049+soft-coded@users.noreply.github.com>
2022-07-19 16:35:25 +0530

    Time: 163 ms (29.56%), Space: 14.2 MB (11.47%) - LeetHub

--- 39-combination-sum/39-combination-sum.py
+++ 39-combination-sum/39-combination-sum.py
@@ -1,16 +1,37 @@
 class Solution:
-    def combinationSum(self, nums: List[int], target: int):
-      ans = []
+  def combinationSum(self, nums: List[int], target: int):
+    ans = []
       
-      def bt(nums, t, cur, ind):
-        if t == 0:
-          ans.append(cur)
-          return
-        if t < 0:
-          return
+#       def bt(nums, t, cur, ind):
+#         if t == 0:
+#           ans.append(cur)
+#           return
+#         if t < 0:
+#           return
         
-        for i in range(ind, len(nums)):
-          bt(nums, t - nums[i], cur + [nums[i]], i)
+#         for i in range(ind, len(nums)):
+#           bt(nums, t - nums[i], cur + [nums[i]], i)
       
-      bt(nums, target, [], 0)
-      return ans
+#       bt(nums, target, [], 0)
+#       return ans
+
+    def recur(i, cursum, path):
+      if cursum == target:
+        return ans.append(path)
+      if i < 0:
+        return
+      
+      # not_pick
+      recur(i - 1, cursum, path)
+      
+      # pick
+      sm = cursum + nums[i]
+      if sm <= target:
+        recur(i, sm, path + [nums[i]])
+    
+    recur(len(nums) - 1, 0, [])
+    return ans     
+      
+      
+      
+      
