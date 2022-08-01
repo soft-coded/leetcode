7a89efcb900026221820431c4de39c1cf32f2d1b
Shrutanten <68508049+soft-coded@users.noreply.github.com>
2022-08-01 23:01:20 +0530

    Time: 159 ms (14.82%), Space: 18.8 MB (22.91%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -6,14 +6,14 @@
 #         self.right = None
 
 class Solution:
-  def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+  def lowestCommonAncestor(self, root: 'TreeNode', P: 'TreeNode', Q: 'TreeNode') -> 'TreeNode':
     if not root:
-      return None
-    if root.val == p.val or root.val == q.val:
+        return None
+    val = root.val
+    if P.val == val or Q.val == val:
       return root
-    if (p.val < root.val and q.val > root.val) or (p.val > root.val and q.val < root.val):
-      return root
-    if p.val < root.val and q.val < root.val:
-      return self.lowestCommonAncestor(root.left, p, q)
-    else:
-      return self.lowestCommonAncestor(root.right, p, q)
+    if P.val < val < Q.val or Q.val < val < P.val:
+        return root
+    if P.val < val and Q.val < val:
+        return self.lowestCommonAncestor(root.left, P, Q)
+    return self.lowestCommonAncestor(root.right, P, Q)
