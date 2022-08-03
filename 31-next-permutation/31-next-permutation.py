686cc42a2b8060cf99803e0dedc45ac470d405d3
Shrutanten <68508049+soft-coded@users.noreply.github.com>
2022-08-03 11:21:11 +0530

    Time: 84 ms (15.96%), Space: 13.8 MB (75.00%) - LeetHub

--- 31-next-permutation/31-next-permutation.py
+++ 31-next-permutation/31-next-permutation.py
@@ -1,23 +1,22 @@
-def reverse(st, end, arr):
-  while st < end:
-    arr[st], arr[end] = arr[end], arr[st]
-    st += 1
-    end -= 1
+def reverse(arr, st, end):
+    while st < end:
+        arr[st], arr[end] = arr[end], arr[st]
+        st += 1
+        end -= 1
 
 class Solution:
-    def nextPermutation(self, nums: List[int]) -> None:
-      n = len(nums)
-      i = n - 2
-      while i >= 0 and nums[i] >= nums[i + 1]:
-        i -= 1
-      
-      if i >= 0:
-        j = n - 1
-        while nums[i] >= nums[j]:
-          j -= 1
-        
-        nums[j], nums[i] = nums[i], nums[j]
-      
-      reverse(i + 1, n - 1, nums)
-      
-      return nums
+  def nextPermutation(self, perm: List[int]) -> None:
+    n = len(perm)
+    i = n - 2
+    while i >= 0 and perm[i] >= perm[i + 1]:
+      i -= 1
+    
+    if i >= 0:
+      j = i + 1
+      while j < n and perm[j] > perm[i]:
+        j += 1
+      j -= 1
+      perm[i], perm[j] = perm[j], perm[i]
+    
+    reverse(perm, i + 1, n - 1)
+    return perm
