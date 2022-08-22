bbf10a01f5c6af98905a0a58c886995c3e092ecb
Shrutanten <68508049+soft-coded@users.noreply.github.com>
2022-08-23 00:18:44 +0530

    Time: 100 ms (7.62%), Space: 16.6 MB (46.94%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.py
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.py
@@ -4,21 +4,27 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-def is_sorted(arr):
-  for i in range(1, len(arr)):
-    if arr[i] <= arr[i - 1]:
-      return False
-  return True
+# def is_sorted(arr):
+#   for i in range(1, len(arr)):
+#     if arr[i] <= arr[i - 1]:
+#       return False
+#   return True
 
 class Solution:
   def isValidBST(self, root: Optional[TreeNode]) -> bool:
-    ino = []
-    def inorder(node):
+    def traverse(node, l, r):
       if not node:
-        return
-      inorder(node.left)
-      ino.append(node.val)
-      inorder(node.right)
+        return True
+      if not (l < node.val < r):
+        return False
       
-    inorder(root)
-    return is_sorted(ino)
+      left = traverse(node.left, l, node.val)
+      right = traverse(node.right, node.val, r)
+      
+      return left and right
+    
+    return traverse(root, -math.inf, math.inf)
+      
+      
+      
+      
