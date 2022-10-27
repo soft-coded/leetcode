094bf0a5f45b37737e05b59b4db2a8ed657b880c
Shrutanten <68508049+soft-coded@users.noreply.github.com>
2022-10-27 10:35:51 +0530

    Time: 385 ms (18.16%), Space: 18.7 MB (35.54%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -0,0 +1,17 @@
+class Solution:
+  def merge(self, intv: List[List[int]]) -> List[List[int]]:
+    intv.sort()
+    ans = []
+    lo = intv[0][0]
+    hi = intv[0][1]
+    
+    for i in range(1, len(intv)):
+      if intv[i][0] <= hi:
+        hi = max(intv[i][1], hi)
+      else:
+        ans.append((lo, hi))
+        lo = intv[i][0]
+        hi = intv[i][1]
+      
+    ans.append((lo, hi))
+    return ans
